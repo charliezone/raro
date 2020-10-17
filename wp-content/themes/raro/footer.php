79e5517b33ea15b58b7a66d3e424271f8890e429
Carlos Rafael <carlitos051186@gmail.com>
2020-10-16 22:01:50 -0400

    set up custom theme

---
+++ wp-content/themes/raro/footer.php
@@ -0,0 +1,100 @@
+<!-- Contacts -->
+
+      <section class="contacts section">
+        <div class="container">
+          <header class="section-header">
+            <h2 class="section-title">Get <span class="text-primary">in touch</span></h2>
+            <strong class="fade-title-right">contacts</strong>
+          </header>
+          <div class="section-content">
+            <div class="row-base row">
+              <div class="col-address col-base col-md-4">
+                +7 (212) 654-33-35<br>
+                +7 (212) 287-85-22<br>
+                info@goarch.com<br>
+                USA, New York, 57 Quigley Bridge
+              </div>
+              <div class="col-base  col-md-8">
+                <form class="js-ajax-form">
+                  <div class="row-field row">
+                    <div class="col-field col-sm-6 col-md-4">
+                      <div class="form-group">
+                        <input type="text" class="form-control" name="name" placeholder="Name">
+                      </div>
+                      <div class="form-group">
+                        <input type="email" class="form-control" name="email" required placeholder="Email *">
+                      </div>
+                    </div>
+                    <div class="col-field col-sm-6 col-md-4">
+                      <div class="form-group">
+                        <input type="tel" class="form-control" name="phone" placeholder="Phone">
+                      </div>
+                      <div class="form-group">
+                        <input type="text" class="form-control" name="company" placeholder="Company">
+                      </div>
+                    </div>
+                    <div class="col-field col-sm-12 col-md-4">
+                      <div class="form-group">
+                        <textarea class="form-control" name="message" placeholder="Message"></textarea>
+                      </div>
+                    </div>
+                    <div class="col-message col-field col-sm-12">
+                      <div class="form-group">
+                        <div class="success-message"><i class="fa fa-check text-primary"></i> Thank you!. Your message is successfully sent...</div>
+                        <div class="error-message">We're sorry, but something went wrong</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-submit text-right"><button type="submit" class="btn btn-shadow-2 wow swing">Send <i class="icon-next"></i></button></div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- Footer -->
+
+      <footer id="footer" class="footer">     
+        <div class="container">
+          <div class="row-base row">
+            <div class="col-base text-left-md col-md-4">
+              <a href="#" class="brand">
+                go<span class="text-primary">.</span>arch
+              </a>
+            </div>
+            <div class="text-center-md col-base col-md-4">
+              <a href="https://themeforest.net/user/murren20" class="author-link">
+                by Murren20
+              </a>
+            </div>
+            <div class="text-right-md col-base col-md-4">
+              © go.arch 2016. All Rights Reserved.
+            </div>
+          </div>
+        </div>
+      </footer>
+
+      <!-- Lines -->
+
+      <div class="page-lines">
+        <div class="container">
+          <div class="col-line col-xs-4">
+            <div class="line"></div>
+          </div>
+          <div class="col-line col-xs-4">
+            <div class="line"></div>
+          </div>
+          <div class="col-line col-xs-4">
+            <div class="line"></div>
+            <div class="line"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<!-- SCRIPTS -->
+<?php wp_footer(); ?> 
+</body>
+</html>
---
+++ wp-content/themes/raro/functions.php
@@ -0,0 +1,51 @@
+<?php
+
+function raro_setup(){
+    add_theme_support( 'post-thumbnails' );
+}
+
+function raro_styles_scripts() {
+    wp_enqueue_style( 'font-oswald', 'https://fonts.googleapis.com/css?family=Oswald:300,400,700');
+    wp_enqueue_style( 'font-montserrat', 'https://fonts.googleapis.com/css?family=Montserrat:400,700');
+    wp_enqueue_style( 'raro-theme-style', get_template_directory_uri() . '/assets/css/style.css', array('font-oswald', 'font-montserrat') );
+    wp_enqueue_style( 'raro-custom-style', get_stylesheet_uri(), array('raro-theme-style') );
+
+    //wp_register_script( 'raro_jquery_version', get_template_directory_uri() . '/assets/js/jquery.min.js', null, '', true );
+    //wp_add_inline_script( 'raro_jquery_version', 'var raro_jquery_version = $.noConflict(true);' );
+
+    wp_enqueue_script( 'script-bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), '', true );
+    wp_enqueue_script( 'script-wow', get_template_directory_uri() . '/assets/js/wow.min.js', array('script-bootstrap'), '', true );
+    wp_enqueue_script( 'script-stellar', get_template_directory_uri() . '/assets/js/jquery.stellar.min.js', array('script-wow'), '', true );
+    wp_enqueue_script( 'script-magnific-popup', get_template_directory_uri() . '/assets/js/jquery.magnific-popup.js', array('script-stellar'), '', true );
+    wp_enqueue_script( 'script-owl-carousel', get_template_directory_uri() . '/assets/js/owl.carousel.min.js', array('script-magnific-popup'), '', true );
+    wp_enqueue_script( 'script-rev-slider-themepunch-tools', get_template_directory_uri() . '/assets/js/rev-slider/jquery.themepunch.tools.min.js', array('script-owl-carousel'), '', true );
+    wp_enqueue_script( 'script-rev-slider-themepunch', get_template_directory_uri() . '/assets/js/rev-slider/jquery.themepunch.revolution.min.js', array('script-rev-slider-themepunch-tools'), '', true );
+    wp_enqueue_script( 'script-rev-slider-extension-actions', get_template_directory_uri() . '/assets/js/rev-slider/revolution.extension.actions.min.js', array('script-rev-slider-themepunch'), '', true );
+    wp_enqueue_script( 'script-rev-slider-extension-carousel', get_template_directory_uri() . '/assets/js/rev-slider/revolution.extension.carousel.min.js', array('script-rev-slider-extension-actions'), '', true );
+    wp_enqueue_script( 'script-rev-slider-extension-kenburn', get_template_directory_uri() . '/assets/js/rev-slider/revolution.extension.kenburn.min.js', array('script-rev-slider-extension-carousel'), '', true );
+    wp_enqueue_script( 'script-rev-slider-extension-layeranimation', get_template_directory_uri() . '/assets/js/rev-slider/revolution.extension.layeranimation.min.js', array('script-rev-slider-extension-kenburn'), '', true );
+    wp_enqueue_script( 'script-rev-slider-extension-migration', get_template_directory_uri() . '/assets/js/rev-slider/revolution.extension.migration.min.js', array('script-rev-slider-extension-layeranimation'), '', true );
+    wp_enqueue_script( 'script-rev-slider-extension-navigation', get_template_directory_uri() . '/assets/js/rev-slider/revolution.extension.navigation.min.js', array('script-rev-slider-extension-migration'), '', true );
+    wp_enqueue_script( 'script-rev-slider-extension-parallax', get_template_directory_uri() . '/assets/js/rev-slider/revolution.extension.parallax.min.js', array('script-rev-slider-extension-navigation'), '', true );
+    wp_enqueue_script( 'script-rev-slider-extension-slideanims', get_template_directory_uri() . '/assets/js/rev-slider/revolution.extension.slideanims.min.js', array('script-rev-slider-extension-parallax'), '', true );
+    wp_enqueue_script( 'script-rev-slider-extension-video', get_template_directory_uri() . '/assets/js/rev-slider/revolution.extension.video.min.js', array('script-rev-slider-extension-slideanims'), '', true );
+    wp_enqueue_script( 'script-interface', get_template_directory_uri() . '/assets/js/interface.js', array('script-rev-slider-extension-video'), '', true );
+}
+
+function raro_head_styles_scripts(){
+    ob_start();
+    ?>
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/favicon.png">
+        <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon-114x114.png">
+    <?php 
+    $content = ob_get_contents();
+    /* Clean buffer */
+    ob_end_clean();
+    echo $content;
+}
+
+add_action( 'after_setup_theme', 'raro_setup' );
+add_action( 'wp_head', 'raro_head_styles_scripts' );
+add_action( 'wp_enqueue_scripts', 'raro_styles_scripts' );
